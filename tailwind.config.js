82bd8f063c9b4f0aebf2e02d3f3ba51bc033aa01
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-01 00:23:45 +0700

    added navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        yellow: {
+          "50": "#FFFDEB",
+          "100": "#FEFAD7",
+          "200": "#FEF5AA",
+          "300": "#FDF181",
+          "400": "#FDEC59",
+          "500": "#FCE72D",
+          "600": "#ECD503",
+          "700": "#B09F02",
+          "800": "#746802",
+          "900": "#3C3601"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
