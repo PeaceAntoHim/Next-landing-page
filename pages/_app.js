a6ee38db253f55a37c1936efe92cb19517064987
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-31 23:05:27 +0700

    get font from google

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 // import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
