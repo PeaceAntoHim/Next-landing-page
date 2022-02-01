f37519fea913a8df6d41c8eef4ddfabffef542bf
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-01 20:27:17 +0700

    made hover for element

--- components/Button.js
+++ components/Button.js
@@ -2,15 +2,15 @@ export default function Button({ className, variant, children, pill }) {
     const addClassName = className ? `${className}` : "";
     
     const variants = {
-        "outline-yellow": `border border-yellow-500 text-yellow-500`,
-        "yellow": "bg-yellow-500 text-black",
-        "black": "bg-black text-white",
+        "outline-yellow": `border border-yellow-500 text-yellow-500 hover:text-black hover:bg-yellow-500`,
+        "yellow": "bg-yellow-500 hover:bg-yellow-600 text-black",
+        "black": "bg-black hover:bg-opacity-90 text-white",
     };
 
     const pickedVariant = variants[variant];
 
     return (
-        <a className={`py-3 px-10 font-semibold inline-block ${pill ? " rounded-full" : ""} ${pickedVariant} ${addClassName}`}>
+        <a className={`transitions py-3 px-10 font-semibold inline-block ${pill ? " rounded-full" : ""} ${pickedVariant} ${addClassName}`}>
             {children}
         </a>
     );
