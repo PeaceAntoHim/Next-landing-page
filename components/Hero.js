ebbdd7591c4d3976b8b039c843d915d83b6eb6e7
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-01 23:10:10 +0700

    made responsive web

--- components/Hero.js
+++ components/Hero.js
@@ -6,12 +6,12 @@ const intro2 = "Since 10 years I have been working on web development and I'm lo
 
 export default function Hero() {
     return (
-        <div className="bg-hero h-[712px]">
-            <div className="container mx-auto">
+            <div className="bg-hero 2xl:h-[712px] xl:h-[600px] lg:h-[550px] md:h-[500px]">
+            <div className="container mx-auto px-10">
                 <Navbar />
-                    <div className="text-center mt-20">
-                        <h1 className="text-3xl text-white font-semibold font-mono w-8/12 mx-auto leading-relaxed">{intro1}</h1>
-                        <p className="text-white text-opacity-60 text-lg w-4/12 mx-auto mt-6 leading-relaxed">{intro2}</p>
+                    <div className="text-center xl:mt-20 lg:mt-10 md:mt-6 pb-20">
+                        <h1 className="text-3xl text-white font-semibold font-mono 2xl:w-6/12 xl:w-8/12 lg:w-10/12 mx-auto leading-relaxed">{intro1}</h1>
+                        <p className="text-white text-opacity-60 text-lg 2xl:w-3/12 xl:w-4/12 lg:w-6/12 mx-auto mt-6 leading-relaxed">{intro2}</p>
                     <Button href="#profile" pill variant="yellow" className="mt-14">Learn</Button>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,10 @@ module.exports = {
     plugin(function({ addUtilities }) {
         const utilities = {
           ".bg-hero": {
-            "background-image": "url(/hero.png)"
+            "background-image": "url(/hero.png)",
+            "background-size": "cover",
+            "background-position": "bottom",
+            "background-repeat": "no-repeat",
           },
           ".shadow-skill": {
             "box-shadow": "0px 15px 30px rgba(0, 0, 0, 0.1)"
