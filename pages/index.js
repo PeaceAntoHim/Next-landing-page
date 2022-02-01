bf3b329c4b9114ee329e7dd78b2dad1e6f6fde06
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-01 15:54:40 +0700

    made sections projects

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import ProfileSection from "../components/ProfileSection";
 import SectionSkills from "../components/SectionSkills";
+import SectionProjects from "../components/SectionProjects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           <Hero />
           <ProfileSection />
           <SectionSkills />
+          <SectionProjects />
       </>
     )
 }
